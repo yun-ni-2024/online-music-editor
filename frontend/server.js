32edc7153685368cd22d52db2bffb49ccbd6e5eb
NigelConkeller <scanocdii@gmail.com>
2024-05-09 18:13:22 +0800

    l

--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(cors());
 
 // 配置静态文件服务，指定静态文件目录为当前目录
 app.use(express.static(path.join(__dirname)));
