66de2db1bc72de750e77b36c0d2f443561ebd0f7
NigelConkeller <scanocdii@gmail.com>
2024-05-09 18:11:30 +0800

    l

--- frontend/message.js
+++ frontend/message.js
@@ -1,14 +1,6 @@
-let config;
-let socket;
-
-fetch('config.json')
-    .then(response => response.json())
-    .then(data => {
-        config = data;
-        initSocket();
-        console.log('Load config: ', data);
-    })
-    .catch(error => console.error('Error loading configuration:', error));
+import {
+    config
+} from './config.js';
 
 import {
     playMusic
@@ -25,69 +17,67 @@ import {
     loadEditMusic
 } from './edit_content.js'
 
-function initSocket() {
-    // 创建 WebSocket 连接
-    socket = new WebSocket(`ws://${config.online ? config.onlineIP : config.offlineIP}:4333`);
+// 创建 WebSocket 连接
+const socket = new WebSocket(`ws://${config.online ? config.onlineIP : config.offlineIP}:4333`);
 
-    // 当连接建立时
-    socket.onopen = function(event) {
-        console.log('WebSocket connection established.');
+// 当连接建立时
+socket.onopen = function(event) {
+    console.log('WebSocket connection established.');
 
-        // 检查是否存在认证令牌
-        const authToken = localStorage.getItem('authToken');
+    // 检查是否存在认证令牌
+    const authToken = localStorage.getItem('authToken');
 
-        if (authToken) {
-            // 用户已登录，可以执行相关操作，例如显示用户信息或访问受限资源
-            console.log('User is logged in');
+    if (authToken) {
+        // 用户已登录，可以执行相关操作，例如显示用户信息或访问受限资源
+        console.log('User is logged in');
 
-            const path = document.location.pathname;
-            console.log('Path = ', path);
+        const path = document.location.pathname;
+        console.log('Path = ', path);
 
-            // 判断当前是哪个页面
-            switch (path) {
-                case '/home':
-                case '/home.html':
-                    fetchHomeMusic();
-                    break;
-                case '/edit':
-                case '/edit.html':
-                    initEditPage();
-                    fetchOpenedMusic();
-                    break;
-                default:
-                    break;
-            }
+        // 判断当前是哪个页面
+        switch (path) {
+            case '/home':
+            case '/home.html':
+                fetchHomeMusic();
+                break;
+            case '/edit':
+            case '/edit.html':
+                initEditPage();
+                fetchOpenedMusic();
+                break;
+            default:
+                break;
         }
-    };
-
-    // 当收到消息时
-    socket.onmessage = function(event) {
-        console.log("Received message:", event.data);
-
-        try {
-            const message = JSON.parse(event.data);
-            switch (message.type) {
-                case 'music':
-                    playMusic(message.data);
-                    break;
-                case 'my music desc':
-                    initHomeMusic(message.data);
-                    break;
-                case 'opened music':
-                    loadEditMusic(message.data);
-                default:
-                    break;
-            }
-        } catch (error) {
-        }
-    };
-}
+    }
+};
 
 function sendMessage(message) {
     const str = JSON.stringify(message);
     socket.send(str);
     console.log("Sent message:", message);
 }
+
+// 当收到消息时
+socket.onmessage = function(event) {
+    console.log("Received message:", event.data);
+
+    try {
+        const message = JSON.parse(event.data);
+        switch (message.type) {
+            case 'music':
+                playMusic(message.data);
+                break;
+            case 'my music desc':
+                initHomeMusic(message.data);
+                break;
+            case 'opened music':
+                loadEditMusic(message.data);
+            default:
+                break;
+        }
+    } catch (error) {
+    }
+};
 
 export {
     sendMessage
