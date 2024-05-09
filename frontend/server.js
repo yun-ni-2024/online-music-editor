df46e3d2d5263695d5f427eb7d7260d46b688dfe
NigelConkeller <scanocdii@gmail.com>
2024-05-09 18:02:17 +0800

    l

--- frontend/server.js
+++ frontend/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const configFile = fs.readFileSync('config.json');
-const config = JSON.parse(configFile);
+// const configFile = fs.readFileSync('config.json');
+// const config = JSON.parse(configFile);
+
+const { config } = require('./config.js');
 
 const app = express();
 
