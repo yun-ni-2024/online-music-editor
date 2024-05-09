21fda99886adece6c2d08d26b7e0c2736b2d898d
NigelConkeller <scanocdii@gmail.com>
2024-05-09 18:31:25 +0800

    l

--- frontend/gallery_content.js
+++ frontend/gallery_content.js
@@ -1,9 +1,3 @@
-let config;
-
-fetch('config.json')
-    .then(response => response.json())
-    .then(data => {
-        config = data;
-        console.log('Load config: ', data);
-    })
-    .catch(error => console.error('Error loading configuration:', error));
+import {
+    config
+} from './config.js';
